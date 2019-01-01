e238776f5fcbe471be9fbda0bd9666d4896c90ea
Scott Parsons <scott@sussexdev.co.uk>
2019-01-01 13:58:53 +0000

    1.0.1

--- UI/DataProvider/DataProvider.php
+++ UI/DataProvider/DataProvider.php
@@ -3,7 +3,7 @@
  * @package    SussexDev_MultiStepModal
  * @copyright  Copyright (c) 2019 Scott Parsons
  * @license    https://github.com/ScottParsons/module-multistepmodal/blob/master/LICENSE.md
- * @version    1.0.0
+ * @version    1.0.1
  */
 namespace SussexDev\MultiStepModal\Ui\DataProvider;
 
